26518d111a3a8565d5a1dcd2c7bb510d88681f07
Berke Batı <berkebati@gmail.com>
2025-05-05 12:32:49 +0300

    added the enemy files and finished ancient level

---
+++ Assets/Scripts/Enemy/BanditController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditController : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float detectionRange = 5f;
+    public float attackRange = 1f;
+    public Transform player;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        if (distanceToPlayer <= attackRange)
+        {
+            animator.SetBool("isPlayerDetected", true);
+            animator.SetBool("isPlayerNear", true);
+            animator.SetBool("isRunning", false);
+            FaceDirection(direction.x);
+            animator.SetTrigger("attack");
+        }
+        else if (distanceToPlayer <= detectionRange)
+        {
+            animator.SetBool("isPlayerNear", false);
+            animator.SetBool("isPlayerDetected", true);
+            animator.SetBool("isRunning", true);
+
+            if (!IsInAnimationState("Attack"))
+            {
+                MoveTowards(direction);
+            }
+        }
+        else
+        {
+            animator.SetBool("isPlayerDetected", false);
+            animator.SetBool("isPlayerNear", false);
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void MoveTowards(Vector2 direction)
+    {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        FaceDirection(direction.x);
+    }
+
+    void FaceDirection(float xDirection)
+    {
+        if (xDirection < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (xDirection > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+    bool IsInAnimationState(string stateName)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+}
---
+++ Assets/Scripts/Enemy/CyberAgentController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyberAgentController : MonoBehaviour
+{
+   public float moveSpeed = 2f;
+    public float detectionRange = 5f;
+    public float attackRange = 0.25f;
+    public Transform player;
+    private Animator animator;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 5f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        if (distanceToPlayer <= attackRange)
+        {
+            animator.SetBool("isPlayerDetected", true);
+            animator.SetBool("isPlayerNear", true);
+            animator.SetBool("isRunning", false);
+            FaceDirection(direction.x);
+            animator.SetTrigger("attack");
+        }
+        else if (distanceToPlayer <= detectionRange)
+        {
+            Debug.Log("????????????????");
+            animator.SetBool("isPlayerNear", false);
+            animator.SetBool("isPlayerDetected", true);
+            animator.SetBool("isRunning", true);
+
+            if (!IsInAnimationState("Attack"))
+            {
+                MoveTowards(direction);
+            }
+        }
+        else
+        {
+            Debug.Log("OOOOOOOOOOOOOOOOOOOOOO");
+            animator.SetBool("isPlayerDetected", false);
+            animator.SetBool("isPlayerNear", false);
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void MoveTowards(Vector2 direction)
+    {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        FaceDirection(direction.x);
+    }
+
+    void FaceDirection(float xDirection)
+    {
+        if (xDirection < 0)
+            transform.localScale = new Vector3(-3, 3, 1);
+        else if (xDirection > 0)
+            transform.localScale = new Vector3(3, 3, 1);
+    }
+    bool IsInAnimationState(string stateName)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    public void ShootProjectile()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        if (rb != null)
+            rb.velocity = direction * projectileSpeed;
+    }
+}
