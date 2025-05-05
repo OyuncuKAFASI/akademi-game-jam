a600f9e78b1c9bdc234292bfd8db1c64baff30f4
Naz Dilber <nazzzzj@gmail.com>
2025-05-05 19:14:37 +0300

    zombie

--- Assets/Scripts/OnTouchRestart.cs
+++ Assets/Scripts/OnTouchRestart.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Lava : MonoBehaviour
 {
     private bool hasCollided = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("character") && !hasCollided)
         {
             hasCollided = true;
-            GameManager.Instance.PlayerDied();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
